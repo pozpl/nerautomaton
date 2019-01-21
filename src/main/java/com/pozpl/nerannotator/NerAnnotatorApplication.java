f21bc7e9b23954c7ba29ed961020b07bd194f1a8
Pavel Pozdnyak <pozdpl@gmail.com>
2019-01-21 23:51:03 +1300

    Added to packaging and build process

---
+++ src/main/java/com/pozpl/nerannotator/NerAnnotatorApplication.java
@@ -0,0 +1,14 @@
+package com.pozpl.nerannotator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NerAnnotatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NerAnnotatorApplication.class, args);
+	}
+
+}
+
