d2327ce12988895f35fafbda7401ef069fad3c23
Pavel Pozdnyak <pozdpl@gmail.com>
2019-02-09 23:18:38 +1300

    User model

---
+++ src/main/java/com/pozpl/nerannotator/persistence/model/User.java
@@ -0,0 +1,142 @@
+package com.pozpl.nerannotator.persistence.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Data
+@Table(name = "users")
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(nullable = false, unique = true)
+	private String username;
+
+	@Column(length = 60)
+	private String password;
+
+	private String firstName;
+	private String lastName;
+	private String email;
+
+	private boolean enabled;
+	private boolean isUsing2FA;
+	private String secret;
+
+
+
+	@ManyToMany
+	@JoinTable(
+			name = "users_roles",
+			joinColumns = @JoinColumn(
+					name = "user_id", referencedColumnName = "id"),
+			inverseJoinColumns = @JoinColumn(
+					name = "role_id", referencedColumnName = "id"))
+	private Collection<Role> roles;
+
+
+	public User() {
+	}
+
+	public User(String username, String password,
+				String firstName,
+				String lastName,
+				String email,
+				boolean enabled,
+				boolean isUsing2FA,
+				Collection<Role> roles) {
+		this.username = username;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.enabled = enabled;
+		this.isUsing2FA = isUsing2FA;
+		this.roles = roles;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+	
+	public Collection<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Collection<Role> roles) {
+		this.roles = roles;
+	}
+
+	public boolean isUsing2FA() {
+		return isUsing2FA;
+	}
+
+	public void setUsing2FA(boolean using2FA) {
+		isUsing2FA = using2FA;
+	}
+
+	public String getSecret() {
+		return secret;
+	}
+
+	public void setSecret(String secret) {
+		this.secret = secret;
+	}
+}
