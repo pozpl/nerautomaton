b4b5a4405e1e3456664c4b66aa522b88f727109b
Pavel Pozdnyak <pozdpl@gmail.com>
2019-02-12 22:50:00 +1300

    DAO layer for user and their rights

---
+++ src/main/java/com/pozpl/nerannotator/persistence/dao/PrivilegeRepository.java
@@ -0,0 +1,14 @@
+package com.pozpl.nerannotator.persistence.dao;
+
+import com.pozpl.nerannotator.persistence.model.Privilege;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PrivilegeRepository extends JpaRepository<Privilege, Long> {
+
+	Privilege findByName(String name);
+
+	@Override
+	void delete(Privilege privilege);
+}
