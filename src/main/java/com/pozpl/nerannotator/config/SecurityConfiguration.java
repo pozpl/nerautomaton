d80065c2d853625623c991d0406181e4eabf913b
Pavel Pozdnyak <pozdpl@gmail.com>
2019-02-12 23:36:18 +1300

    experiments with security context

--- src/main/java/com/pozpl/nerannotator/config/SecurityConfiguration.java
+++ src/main/java/com/pozpl/nerannotator/config/SecurityConfiguration.java
@@ -1,10 +1,15 @@
 package com.pozpl.nerannotator.config;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import com.pozpl.nerannotator.service.MyUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
@@ -19,9 +24,59 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.authorizeRequests()
 				.antMatchers("/index.html", "/", "/home", "/login")
 				.permitAll()
+				.antMatchers("/admin/**").hasAuthority("ADMIN")
 				.antMatchers("/*.js", "/*.css", "/*.ico").permitAll()
 				.anyRequest().authenticated()
 				.and().csrf()
 				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+
+		/*
+		http.
+                authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/login").permitAll()
+                .antMatchers("/registration").permitAll()
+                .antMatchers("/admin/**").hasAuthority("ADMIN").anyRequest()
+                .authenticated().and().csrf().disable().formLogin()
+                .loginPage("/login").failureUrl("/login?error=true")
+                .defaultSuccessUrl("/admin/home")
+                .usernameParameter("email")
+                .passwordParameter("password")
+                .and().logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/").and().exceptionHandling()
+                .accessDeniedPage("/access-denied");
+
+		 */
+	}
+
+//	@Override
+//	public void configure(WebSecurity web) throws Exception {
+//		web.ignoring()
+//			.antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
+//	}
+
+
+	@Autowired
+	private MyUserDetailService userDetailsService;
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth)
+			throws Exception {
+		auth.authenticationProvider(authenticationProvider());
+	}
+
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authProvider
+				= new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(userDetailsService);
+		authProvider.setPasswordEncoder(encoder());
+		return authProvider;
+	}
+
+	@Bean
+	public PasswordEncoder encoder() {
+		return new BCryptPasswordEncoder(11);
 	}
 }
