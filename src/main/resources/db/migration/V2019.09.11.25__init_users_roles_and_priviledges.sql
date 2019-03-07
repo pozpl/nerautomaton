e7a35e887ca19863cb14a6771bbab05cc0a74149
Pavel Pozdnyak <pozdpl@gmail.com>
2019-03-08 00:20:23 +1300

    Get out of mysql to postgres

--- src/main/resources/db/migration/V2019.09.11.25__init_users_roles_and_priviledges.sql
+++ src/main/resources/db/migration/V2019.09.11.25__init_users_roles_and_priviledges.sql
@@ -1,21 +1,25 @@
 BEGIN;
 
-INSERT IGNORE INTO users(username, password, first_name, last_name, email, enabled, is_using_2fa, secret ) VALUES
-("admin", "$2y$11$LWtlxi2Z6d7W3TJOf5GPNeptEbVyojtPxz.bliT2Pd4jXhH7Yg.Hi",
-"Admin", "Admin", "Admin Email", true, false, NULL );
+INSERT INTO users(username, password, first_name, last_name, email, enabled, is_using_2fa, secret ) VALUES
+('admin', '$2y$11$LWtlxi2Z6d7W3TJOf5GPNeptEbVyojtPxz.bliT2Pd4jXhH7Yg.Hi',
+'Admin', 'Admin', 'Admin Email', true, false, NULL )
+ON CONFLICT(username) DO NOTHING;
 
-INSERT IGNORE INTO roles(`name`) VALUES
-("ROLE_USER"),
-("ROLE_ADMIN"),
-("ROLE_PRIVILEGED_USER");
+INSERT INTO roles(name) VALUES
+('ROLE_USER'),
+('ROLE_ADMIN'),
+('ROLE_PRIVILEGED_USER')
+ON CONFLICT(name) DO NOTHING;
 
-INSERT IGNORE INTO privileges(`name`) VALUES
-("READ_PRIVILEGE"),
-("WRITE_PRIVILEGE");
+INSERT INTO privileges(name) VALUES
+('READ_PRIVILEGE'),
+('WRITE_PRIVILEGE')
+ON CONFLICT(name) DO NOTHING;
 
-INSERT IGNORE INTO users_roles(user_id, role_id)
+INSERT INTO users_roles(user_id, role_id)
 SELECT u.id, r.id FROM users AS u
-JOIN roles AS r ON r.name = "ROLE_ADMIN"
-WHERE username = "admin";
+JOIN roles AS r ON r.name = 'ROLE_ADMIN'
+WHERE username = 'admin'
+ON CONFLICT(user_id, role_id) DO NOTHING;
 
 COMMIT;
