08808bd2e1151420ba71ecfe50384caa039c8816
Pavel Pozdnyak <pozdpl@gmail.com>
2019-03-12 23:05:09 +1300

    Spring configuration for tests. More direct one that via application-test properties

--- src/test/java/com/pozpl/nerannotator/NerAnnotatorApplicationTests.java
+++ src/test/java/com/pozpl/nerannotator/NerAnnotatorApplicationTests.java
@@ -1,16 +1,37 @@
 package com.pozpl.nerannotator;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import javax.sql.DataSource;
+
+
+@Configuration
+@EnableJpaRepositories(basePackages = "com.pozpl.nerannotator.persistence.dao")
+@PropertySource("persistence.properties")
+@EnableTransactionManagement
+@SpringBootApplication(scanBasePackages = {"com.pozpl.nerannotator.persistence", "com.pozpl.nerannotator.service"})
 public class NerAnnotatorApplicationTests {
 
-	@Test
-	public void contextLoads() {
+	@Autowired
+	private Environment env;
+
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
+		dataSource.setUrl(env.getProperty("jdbc.url"));
+//        dataSource.setUsername(env.getProperty("jdbc.user"));
+//        dataSource.setPassword(env.getProperty("jdbc.pass"));
+
+		return dataSource;
 	}
 
 }
