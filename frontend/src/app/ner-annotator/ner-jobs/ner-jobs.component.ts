c998365b8a9f185de260cd4f3e64a61ed76855fd
Pavel Pozdnyak <pozdpl@gmail.com>
2019-03-30 13:07:40 +1300

    Started to tinker with ner job component. Getting trough data source concept that is used for table rendering in material design library

---
+++ frontend/src/app/ner-annotator/ner-jobs/ner-jobs.component.ts
@@ -0,0 +1,54 @@
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "../../auth.service";
+import {MatDialog} from "@angular/material";
+import {NerJobsService, Page} from "./ner-jobs.service";
+import {NerJobDto} from "./ner-job.dto";
+import {DataSource} from "@angular/cdk/table";
+import {Observable} from "rxjs";
+import {map} from "rxjs/operators";
+
+@Component({
+    selector: 'app-ner-jobs',
+    templateUrl: './ner-jobs.component.html',
+    styleUrls: ['./ner-jobs.component.scss']
+})
+export class NerJobsComponent implements OnInit {
+
+    currentPageContent: NerJobDto[];
+    pagedContent: Page<NerJobDto>;
+
+    nerJobsDataSource: NerJobsDataSource;
+
+    constructor(public auth: AuthService,
+                public nerJobsService: NerJobsService,
+                public dialog: MatDialog) {
+    }
+
+    ngOnInit() {
+
+        this.nerJobsService.getJobs(1).subscribe((page) => {
+            this.pagedContent = page;
+        });
+    }
+
+    goToAddEditJob() {
+
+    }
+
+}
+
+
+
+export class NerJobsDataSource extends DataSource<any> {
+    constructor(private dataService: NerJobsService) {
+        super();
+    }
+
+    connect(): Observable<NerJobDto[]> {
+        return this.dataService.getJobs(1)
+            .pipe(map(pageContext => pageContext.content));
+    }
+
+    disconnect() {
+    }
+}
