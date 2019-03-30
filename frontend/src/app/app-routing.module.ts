a736707edcd03bd4ec4814c1615e0ddcc0c7bd2e
Pavel Pozdnyak <pozdpl@gmail.com>
2019-03-30 14:23:43 +1300

    Routing added to ner annotation module

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,30 +1,30 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {LoginComponent} from "./login/login.component";
-import {combineAll} from "rxjs/operators";
 import {HeroesComponent} from "./heroes/heroes.component";
-import {DashboardComponent} from "./dashboard/dashboard.component";
 import {HeroDetailComponent} from "./hero-detail/hero-detail.component";
 import {TextItemComponent} from "./ner-annotator/text-item/text-item.component";
-import {NerAnnotatorModule} from "./ner-annotator/ner-annotator.module";
 import {NerJobsComponent} from "./ner-annotator/ner-jobs/ner-jobs.component";
+import {NerJobEditComponent} from "./ner-annotator/ner-jobs/ner-job-edit/ner-job-edit.component";
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'dashboard'},
-  { path: 'home', component: HomeComponent},
-  { path: 'login', component: LoginComponent},
-  { path: 'heroes', component: HeroesComponent},
-  { path: 'dashboard', component: NerJobsComponent},
-  { path: 'detail/:id', component: HeroDetailComponent },
-  { path: 'ner/text-item', component: TextItemComponent }
+    {path: '', pathMatch: 'full', redirectTo: 'dashboard'},
+    {path: 'home', component: HomeComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'heroes', component: HeroesComponent},
+    {path: 'ner/jobs', component: NerJobsComponent},
+    {path: 'ner/jobs', component: NerJobsComponent},
+    {path: 'ner/job/edit/?:id', component: NerJobEditComponent},
+    {path: 'detail/:id', component: HeroDetailComponent},
+    {path: 'ner/text-item', component: TextItemComponent}
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, {useHash: true}),
-    NerAnnotatorModule
-  ],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(routes, {useHash: true})
+    ],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- frontend/src/app/ner-annotator/ner-annotator.module.ts
+++ frontend/src/app/ner-annotator/ner-annotator.module.ts
@@ -1,33 +1,35 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { TextItemComponent } from './text-item/text-item.component';
+import {TextItemComponent} from './text-item/text-item.component';
 import {MaterialModule} from "../material.module";
 import {FlexLayoutModule} from "@angular/flex-layout";
 import {TextSelectDirective} from "./text-item/text-select.directive";
 import {TermsAnnotationsService} from "./text-item/terms-annotations.service";
-import {ResultsDataService} from "./text-item/results-data.service";
-import { ResultsReviewComponent } from './results-review/results-review.component';
-import { NerJobsComponent } from './ner-jobs/ner-jobs.component';
+import {ResultsReviewComponent} from './results-review/results-review.component';
+import {NerJobsComponent} from './ner-jobs/ner-jobs.component';
 import {NerJobsService} from "./ner-jobs/ner-jobs.service";
-import { NerJobEditComponent } from './ner-jobs/ner-job-edit/ner-job-edit.component';
+import {NerJobEditComponent} from './ner-jobs/ner-job-edit/ner-job-edit.component';
+import {AppRoutingModule} from "../app-routing.module";
 
 @NgModule({
-  declarations: [
-    TextItemComponent,
-    TextSelectDirective,
-    ResultsReviewComponent,
-    NerJobsComponent,
-    NerJobEditComponent
-  ],
-  imports: [
-    CommonModule,
-    MaterialModule,
-    FlexLayoutModule
-  ],
-  providers: [
-      TermsAnnotationsService,
-      NerJobsService
-  ]
+    declarations: [
+        TextItemComponent,
+        TextSelectDirective,
+        ResultsReviewComponent,
+        NerJobsComponent,
+        NerJobEditComponent
+    ],
+    imports: [
+        CommonModule,
+        MaterialModule,
+        FlexLayoutModule,
+        AppRoutingModule
+    ],
+    providers: [
+        TermsAnnotationsService,
+        NerJobsService
+    ]
 })
-export class NerAnnotatorModule { }
+export class NerAnnotatorModule {
+}
