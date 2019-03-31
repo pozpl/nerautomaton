e7cc7df6dd0b7026ec786d89ef27fe4eeff0eebb
Pavel Pozdnyak <pozdpl@gmail.com>
2019-03-31 14:28:05 +1300

    It's important to unsubscribe

--- frontend/src/app/ner-annotator/ner-jobs/ner-job-edit/ner-job-edit.component.ts
+++ frontend/src/app/ner-annotator/ner-jobs/ner-job-edit/ner-job-edit.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {NerJobsService} from "../ner-jobs.service";
+import {Subject} from "rxjs";
+import {takeUntil} from "rxjs/operators";
 
 @Component({
-  selector: 'ner-job-edit',
-  templateUrl: './ner-job-edit.component.html',
-  styleUrls: ['./ner-job-edit.component.scss']
+    selector: 'ner-job-edit',
+    templateUrl: './ner-job-edit.component.html',
+    styleUrls: ['./ner-job-edit.component.scss']
 })
-export class NerJobEditComponent implements OnInit {
+export class NerJobEditComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+    private unsubscribe: Subject<void> = new Subject();
+    jobId: number;
 
-  ngOnInit() {
-  }
+    constructor(private route: ActivatedRoute,
+                private router: Router,
+                private nerJobService: NerJobsService) {
+    }
+
+    ngOnInit() {
+        this.route
+            .params
+            .pipe(takeUntil(this.unsubscribe))
+            .subscribe(params => {
+                this.jobId = params['id'];
+                if (this.jobId != null) {
+                    this.nerJobService.getJob(this.jobId)
+                        .pipe(takeUntil(this.unsubscribe))
+                        .subscribe()
+                }
+            });
+    }
+
+    ngOnDestroy() {
+        this.unsubscribe.next();
+        this.unsubscribe.complete();
+    }
 
 }
