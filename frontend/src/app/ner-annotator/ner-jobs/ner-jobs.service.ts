72f803230e7cc016fd3464f116dcea8ad8e494c6
Pavel Pozdnyak <pozdpl@gmail.com>
2019-03-31 14:29:23 +1300

    Method to get one job

--- frontend/src/app/ner-annotator/ner-jobs/ner-jobs.service.ts
+++ frontend/src/app/ner-annotator/ner-jobs/ner-jobs.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {NerJobDto} from "./ner-job.dto";
@@ -6,15 +6,23 @@ import {NerJobDto} from "./ner-job.dto";
 @Injectable()
 export class NerJobsService {
 
-  constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) {
+    }
 
 
-  getJobs(page: number): Observable<Page<NerJobDto>>{
-      return this.http.get<Page<NerJobDto>>('/ner/edit/list', {
-          params: new HttpParams().set('page', page.toString()),
-          responseType: 'json'
-      });
-  }
+    getJobs(page: number): Observable<Page<NerJobDto>> {
+        return this.http.get<Page<NerJobDto>>('/ner/edit/list', {
+            params: new HttpParams().set('page', page.toString()),
+            responseType: 'json'
+        });
+    }
+
+    getJob(jobId): Observable<NerJobDto> {
+        return this.http.get<NerJobDto>('/ner/edit/list', {
+            params: new HttpParams().set('id', jobId),
+            responseType: 'json'
+        });
+    }
 }
 
 
