48f85e21627c13f42eb87e3ad07d28b6c1499bc9
Pavel Pozdnyak <pozdpl@gmail.com>
2019-08-01 00:12:40 +1200

    Service method to get texts for job

---
+++ src/main/java/com/pozpl/nerannotator/service/ner/text/JobTextAccessServiceImpl.java
@@ -0,0 +1,67 @@
+package com.pozpl.nerannotator.service.ner.text;
+
+import com.pozpl.nerannotator.persistence.dao.job.LabelingJobsRepository;
+import com.pozpl.nerannotator.persistence.dao.ner.NerJobTextItemRepository;
+import com.pozpl.nerannotator.persistence.model.User;
+import com.pozpl.nerannotator.persistence.model.job.LabelingJob;
+import com.pozpl.nerannotator.persistence.model.ner.NerJobTextItem;
+import com.pozpl.nerannotator.service.exceptions.NerServiceException;
+import io.vavr.control.Option;
+import io.vavr.control.Try;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class JobTextAccessServiceImpl implements IJobTextAccessService {
+
+	private final NerJobTextItemRepository nerJobTextItemRepository;
+	private final LabelingJobsRepository labelingJobsRepository;
+
+	@Autowired
+	public JobTextAccessServiceImpl(NerJobTextItemRepository nerJobTextItemRepository,
+									LabelingJobsRepository labelingJobsRepository) {
+		this.nerJobTextItemRepository = nerJobTextItemRepository;
+		this.labelingJobsRepository = labelingJobsRepository;
+	}
+
+	/**
+	 * List texts for job
+	 *
+	 * @param jobId
+	 * @param page
+	 * @return
+	 * @throws NerServiceException
+	 */
+	@Override
+	public Page<JobTextDto> getTextForJob(final Integer jobId,
+										  final Integer page,
+										  final User jobOwner) throws NerServiceException {
+		try {
+			final Optional<LabelingJob> jobOpt = labelingJobsRepository.findByIdAndOwner(Long.valueOf(jobId), jobOwner);
+
+			if(jobOpt.isPresent()){
+				if(! jobOpt.get().getOwner().equals(jobOwner)){
+					 throw new NerServiceException("Access violation for Ner Job " + jobId + " and user: " + jobOwner.getUsername());
+				}
+
+				Page<NerJobTextItem> textItems = this.nerJobTextItemRepository.getForJob(jobOpt.get(),
+									PageRequest.of(0, 20, Sort.by(Sort.Direction.ASC, "id")));
+				return textItems.map(textItem -> JobTextDto.builder()
+						.id(textItem.getId().intValue())
+						.text(textItem.getText()).build()
+				);
+			}
+
+			return Page.empty();
+		}catch (Exception e){
+			throw new NerServiceException(e);
+		}
+	}
+}
