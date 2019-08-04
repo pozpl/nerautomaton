70de2af30fb543bb50544c7a7b7bb0803c3c7cab
Pavel Pozdnyak <pozdpl@gmail.com>
2019-08-04 22:40:55 +1200

    Lombock will do the trick with constructors builders getters and setters

--- src/main/java/com/pozpl/nerannotator/persistence/model/ner/NerJobTextItem.java
+++ src/main/java/com/pozpl/nerannotator/persistence/model/ner/NerJobTextItem.java
@@ -1,13 +1,19 @@
 package com.pozpl.nerannotator.persistence.model.ner;
 
 import com.pozpl.nerannotator.persistence.model.job.LabelingJob;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.Calendar;
 
 
 @Data
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "ner_jobs_texts",
 		uniqueConstraints = { @UniqueConstraint(columnNames = { "job_id", "md5_hash" }) })
@@ -41,89 +47,5 @@ public class NerJobTextItem {
 	@Temporal(value = TemporalType.TIMESTAMP)
 	@Column(name = "updated", nullable = false)
 	private Calendar updated;
-
-	public NerJobTextItem() {
-	}
-
-	public NerJobTextItem(LabelingJob job, String text, String md5Hash) {
-		this.job = job;
-		this.text = text;
-		this.md5Hash = md5Hash;
-		this.created = Calendar.getInstance();
-		this.updated = this.created;
-	}
-
-	public NerJobTextItem(LabelingJob job, String text, String md5Hash, String tokens) {
-		this.job = job;
-		this.text = text;
-		this.md5Hash = md5Hash;
-		this.tokens = tokens;
-		this.created = Calendar.getInstance();
-		this.updated = this.created;
-	}
-
-	public NerJobTextItem(LabelingJob job, String text, String md5Hash, String tokens, Calendar created, Calendar updated) {
-		this.job = job;
-		this.text = text;
-		this.md5Hash = md5Hash;
-		this.tokens = tokens;
-		this.created = created;
-		this.updated = updated;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public LabelingJob getJob() {
-		return job;
-	}
-
-	public void setJob(LabelingJob job) {
-		this.job = job;
-	}
-
-	public String getText() {
-		return text;
-	}
-
-	public void setText(String text) {
-		this.text = text;
-	}
-
-	public String getMd5Hash() {
-		return md5Hash;
-	}
-
-	public void setMd5Hash(String md5Hash) {
-		this.md5Hash = md5Hash;
-	}
-
-	public String getTokens() {
-		return tokens;
-	}
-
-	public void setTokens(String tokens) {
-		this.tokens = tokens;
-	}
-
-	public Calendar getCreated() {
-		return created;
-	}
-
-	public void setCreated(Calendar created) {
-		this.created = created;
-	}
-
-	public Calendar getUpdated() {
-		return updated;
-	}
-
-	public void setUpdated(Calendar updated) {
-		this.updated = updated;
-	}
+	
 }
