d0951f18664b2ae1f5033eb48e012c7ea4e2556c
Pavel Pozdnyak <pozdpl@gmail.com>
2019-08-06 23:51:40 +1200

    Updated angular and added couple of ner job edit dialog

--- frontend/src/app/ner-annotator/ner-job-text-edit/ner-job-text-edit.component.ts
+++ frontend/src/app/ner-annotator/ner-job-text-edit/ner-job-text-edit.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Inject} from '@angular/core';
+import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
 
 @Component({
-  selector: 'app-ner-job-text-edit',
-  templateUrl: './ner-job-text-edit.component.html',
-  styleUrls: ['./ner-job-text-edit.component.scss']
+    selector: 'ner-job-text-edit',
+    templateUrl: './ner-job-text-edit.component.html',
+    styleUrls: ['./ner-job-text-edit.component.scss']
 })
 export class NerJobTextEditComponent implements OnInit {
 
-  constructor() { }
+    constructor(public dialog: MatDialog) {
+    }
 
-  ngOnInit() {
-  }
+    openDialog() {
+        this.dialog.open(NerJobTextEditDialog, {
+            data: {
+                animal: 'panda'
+            }
+        });
+    }
+
+    ngOnInit() {
+    }
 
 }
+
+export interface DialogData {
+    animal: 'panda' | 'unicorn' | 'lion';
+}
+
+@Component({
+    selector: 'ner-job-edit-dialog',
+    templateUrl: 'ner-job-edit-dialog.component.html',
+})
+export class NerJobTextEditDialog {
+    constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData) {
+    }
+}
+
+// @Component({
+//     selector: 'dialog-data-example-dialog',
+//     templateUrl: 'dialog-data-example-dialog.html',
+// })
+// export class DialogDataExampleDialog {
+//     constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+// }
