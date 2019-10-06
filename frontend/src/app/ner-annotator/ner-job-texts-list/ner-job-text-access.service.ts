7355770ed6e35183cde75acff7d40173d37b2865
Pavel Pozdnyak <pozdpl@gmail.com>
2019-10-07 00:04:36 +1300

    Component to show list of texts related to the Job

--- frontend/src/app/ner-annotator/ner-job-texts-list/ner-job-text-access.service.ts
+++ frontend/src/app/ner-annotator/ner-job-texts-list/ner-job-text-access.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from "@angular/core";
 import {NerJobTextDto} from "./ner-job-text.dto";
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {Page} from "../ner-jobs/ner-jobs.service";
 
 
 @Injectable()
@@ -11,8 +12,8 @@ export class NerJobTextAccessService {
 
     }
 
-    listText(jobId: number, page:number):Observable<NerJobTextDto>{
-        return this.httpClient.get<NerJobTextDto>("/ner/text/list", {
+    listText(jobId: number, page:number):Observable<Page<NerJobTextDto>>{
+        return this.httpClient.get<Page<NerJobTextDto>>("/ner/text/list", {
             params: new HttpParams().set('page', page.toString())
                         .append('jobId', jobId.toString()),
             responseType: 'json'
--- frontend/src/app/ner-annotator/ner-job-texts-list/ner-job-texts-list.component.ts
+++ frontend/src/app/ner-annotator/ner-job-texts-list/ner-job-texts-list.component.ts
@@ -1,19 +1,26 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {Page} from "../ner-jobs/ner-jobs.service";
 import {NerJobTextDto} from "./ner-job-text.dto";
+import {NerJobDto} from "../ner-jobs/ner-job.dto";
+import {NerJobTextAccessService} from "./ner-job-text-access.service";
+import {Observable} from "rxjs";
 
 @Component({
-    selector: 'app-ner-job-texts-list',
+    selector: 'ner-job-texts-list',
     templateUrl: './ner-job-texts-list.component.html',
-    styleUrls: ['./ner-job-texts-list.component.scss']
+    styleUrls: ['./ner-job-texts-list.component.scss'],
+    providers: [NerJobTextAccessService]
 })
-export class NerJobTextsListComponent implements OnInit {
+export class NerJobTextsListComponent implements OnInit, OnChanges {
+
+    @Input() job: NerJobDto;
 
     displayedColumns = ["seqNo", "text", "delete"];
 
-    pagedContent: Page<NerJobTextDto>;
+    pagedContent$: Observable<Page<NerJobTextDto>>;
+    page: number = 1;
 
-    constructor() {
+    constructor(private textAccessService: NerJobTextAccessService) {
     }
 
     ngOnInit() {
@@ -22,5 +29,11 @@ export class NerJobTextsListComponent implements OnInit {
 
     deleteText(textDto: NerJobTextDto){
 
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if (this.job  !== null){
+            this.pagedContent$ = this.textAccessService.listText(this.job.id, this.page);
+        }
     }
 }
