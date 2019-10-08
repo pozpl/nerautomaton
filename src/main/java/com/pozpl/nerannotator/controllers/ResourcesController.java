b07cc93e03650ae41af3025ccd12d45e236654f5
Pavel Pozdnyak <pozdpl@gmail.com>
2019-10-08 23:06:08 +1300

    deleted unneeded route

--- src/main/java/com/pozpl/nerannotator/controllers/ResourcesController.java
+++ src/main/java/com/pozpl/nerannotator/controllers/ResourcesController.java
@@ -12,14 +12,6 @@ import java.util.UUID;
 public class ResourcesController {
 
 
-	@RequestMapping("/resource")
-	public Map<String,Object> home() {
-		Map<String,Object> model = new HashMap<String,Object>();
-		model.put("id", UUID.randomUUID().toString());
-		model.put("content", "Hello World");
-		return model;
-	}
-
 	@RequestMapping("/user")
 	public Principal user(Principal user) {
 		return user;
