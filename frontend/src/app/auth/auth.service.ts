6862ce44c9c4d58268d4cefe66d023fb8fa1b975
Pavel Pozdnyak <pozdpl@gmail.com>
2019-10-08 23:05:41 +1300

    Return user observable

--- frontend/src/app/auth/auth.service.ts
+++ frontend/src/app/auth/auth.service.ts
@@ -1,62 +1,72 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import { MatDialog } from "@angular/material/dialog";
+import {MatDialog} from "@angular/material/dialog";
 import {ErrorDialogComponent} from "../shared-components/error-dialog/error-dialog.component";
+import {UserDto} from "./user.dto.";
+import {Observable} from "rxjs";
+import {map} from "rxjs/operators";
 
 
 @Injectable()
 export class AuthService {
 
-  private _authenticated = false;
+    private _authenticated = false;
+    private _user: UserDto = null;
 
-  constructor(private http: HttpClient,
-              private  dialog: MatDialog) {
-  }
+    constructor(private http: HttpClient,
+                private  dialog: MatDialog) {
+    }
 
-  get authenticated(): boolean {
-    return this._authenticated;
-  }
+    get authenticated(): boolean {
+        return this._authenticated;
+    }
 
 
-  set authenticated(value: boolean) {
-    this._authenticated = value;
-  }
+    set authenticated(value: boolean) {
+        this._authenticated = value;
+    }
 
-  authenticate(credentials, callback) {
+    authenticate(credentials, callback) {
 
-    const headers = new HttpHeaders(credentials ? {
-      authorization: 'Basic ' + btoa(credentials.username + ':' + credentials.password)
-    } : {});
+        const headers = new HttpHeaders(credentials ? {
+            authorization: 'Basic ' + btoa(credentials.username + ':' + credentials.password)
+        } : {});
 
-    this.http.get('/login', {headers: headers}).subscribe(response => {
-        if (response['name']) {
-          this._authenticated = true;
-        } else {
-          this._authenticated = false;
-        }
-        return callback && callback();
-      },
-      error => {
-        this.dialog.open(ErrorDialogComponent, {
-          data: {
-            message: "Your login information are incorrect!"
-          }
-        });
-      } // error path
-    );
+        this.http.get('/login', {headers: headers}).subscribe(response => {
+                if (response['name']) {
+                    this._authenticated = true;
+                } else {
+                    this._authenticated = false;
+                }
+                return callback && callback();
+            },
+            error => {
+                this.dialog.open(ErrorDialogComponent, {
+                    data: {
+                        message: "Your login information are incorrect!"
+                    }
+                });
+            } // error path
+        );
 
-  }
+    }
 
-  isLoggedIn() {
+    isLoggedIn(): Observable<UserDto> {
 
-    this.http.get('/user', {}).subscribe(response => {
-        if (response['name']) {
-          this._authenticated = true;
-        } else {
-          this._authenticated = false;
-        }
-      }
-    );
-  }
+        return this.http.get<UserDto>('/user', {})
+            .pipe(
+                map(response => {
+                    if (response.name) {
+                        this._authenticated = true;
+                    } else {
+                        this._authenticated = false;
+                    }
+
+                    this._user = response;
+
+                    return response;
+                })
+            );
+    }
 
 }
