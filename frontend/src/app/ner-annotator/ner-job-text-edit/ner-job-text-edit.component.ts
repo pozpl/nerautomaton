b897662714ee97981627ee481cb6981c99c586e3
Pavel Pozdnyak <pozdpl@gmail.com>
2019-10-23 00:11:14 +1300

    Edit component with modal window

--- frontend/src/app/ner-annotator/ner-job-text-edit/ner-job-text-edit.component.ts
+++ frontend/src/app/ner-annotator/ner-job-text-edit/ner-job-text-edit.component.ts
@@ -1,20 +1,26 @@
 import {Component, OnInit, Inject, Input, Output, EventEmitter} from '@angular/core';
 import {MatDialog, MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import {NerJobTextDto} from "../ner-job-texts-list/ner-job-text.dto";
-import {FormBuilder, FormGroup} from "@angular/forms";
 import {NerJobTextEditDialog} from "./ner-job-text-edit-dialog.component";
+import {NerJobTextEditService} from "./ner-job-text-edit.service";
+import {flatMap} from "rxjs/operators";
+import {of} from "rxjs";
+import {NerJobDto} from "../ner-jobs/ner-job.dto";
 
 @Component({
     selector: 'ner-job-text-edit',
     templateUrl: './ner-job-text-edit.component.html',
-    styleUrls: ['./ner-job-text-edit.component.scss']
+    styleUrls: ['./ner-job-text-edit.component.scss'],
+    providers: [NerJobTextEditService]
 })
 export class NerJobTextEditComponent implements OnInit {
 
-    @Input() textDto: NerJobTextDto;
+    @Input("textDto") textDto: NerJobTextDto;
+    @Input("job") jobDto: NerJobDto;
     @Output() savedText: EventEmitter<NerJobTextDto>;
 
-    constructor(public dialog: MatDialog) {
+    constructor(public dialog: MatDialog,
+                private textService: NerJobTextEditService) {
     }
 
 
@@ -22,23 +28,38 @@ export class NerJobTextEditComponent implements OnInit {
         this.editText(this.textDto);
     }
 
-    editText(textDto: NerJobTextDto){
+    editText(textDto: NerJobTextDto) {
         let dialogRef = this.dialog.open(NerJobTextEditDialog, {
             minHeight: 500,
             minWidth: 500,
 
-            data: {
-                text: textDto
-            }
+            data: textDto
         });
 
-        dialogRef.afterClosed().subscribe(
-            data => console.log("Dialog output:", data)
-        );
+        dialogRef.afterClosed().pipe(
+            flatMap(textDto => {
+                if (textDto !== null) {
+                    return this.textService.save(textDto);
+                }
+                return of(null);
+            })
+        )
+            .subscribe(
+                textDto => {
+                    if (textDto !== null) {
+                        this.textDto = textDto.jobTextDto;
+                    }
+                },
+                cancel => console.log("closed with error")
+            );
     }
 
-    createTextItem(){
-        this.editText(new NerJobTextDto());
+    createTextItem() {
+        if (this.jobDto !== null && this.jobDto.id) {
+            const textToAdd = new NerJobTextDto();
+            textToAdd.jobId = this.jobDto.id;
+            this.editText(textToAdd);
+        }
     }
 
 
