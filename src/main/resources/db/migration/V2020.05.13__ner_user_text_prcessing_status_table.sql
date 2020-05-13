72775352a6593547fdde143e78507f322f3be584
Pavel Pozdnyak <pozdpl@gmail.com>
2020-05-13 23:33:00 +1200

    migration for user text processing status table creation.

---
+++ src/main/resources/db/migration/V2020.05.13__ner_user_text_prcessing_status_table.sql
@@ -0,0 +1,17 @@
+BEGIN;
+
+
+CREATE TABLE IF NOT EXISTS ner_user_text_processing_status
+(
+    id          SERIAL,
+    user_id     INT NOT NULL ,
+    text_id      INT          NOT NULL,
+    created     timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated     timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    CONSTRAINT nutps_user_id_text_id_uc UNIQUE (user_id, text_id),
+    CONSTRAINT nutps_user_id_fk FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    CONSTRAINT nutps_text_id_fk FOREIGN KEY (text_id) REFERENCES ner_jobs_texts (id) ON DELETE CASCADE
+);
+
+COMMIT;
