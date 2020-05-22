e3b1f7d8063f12b9c4381cbc86a2be85cb11702c
Pavel Pozdnyak <pozdpl@gmail.com>
2020-05-23 11:22:58 +1200

    Reorganised ner client side code in for management and annotation parts

--- frontend/src/app/ner-annotator/ner-annotator.module.ts
+++ frontend/src/app/ner-annotator/ner-annotator.module.ts
@@ -1,20 +1,21 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
-import {TextItemComponent} from './text-item/text-item.component';
+
 import {MaterialModule} from "../material.module";
 import {FlexLayoutModule} from "@angular/flex-layout";
-import {TextSelectDirective} from "./text-item/text-select.directive";
-import {TermsAnnotationsService} from "./text-item/terms-annotations.service";
-import {ResultsReviewComponent} from './results-review/results-review.component';
-import {NerJobsComponent} from './ner-jobs/ner-jobs.component';
-import {NerJobsService} from "./ner-jobs/ner-jobs.service";
-import {NerJobEditComponent} from './ner-jobs/ner-job-edit/ner-job-edit.component';
 import {AppRoutingModule} from "../app-routing.module";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import { NerJobTextsListComponent } from './ner-job-texts-list/ner-job-texts-list.component';
-import {NerJobTextAddComponent} from './ner-job-text-edit/ner-job-text-add.component';
-import {NerJobTextEditDialog} from "./ner-job-text-edit/ner-job-text-edit-dialog.component";
+import {TextItemComponent} from "./annotation/text-item/text-item.component";
+import {TextSelectDirective} from "./annotation/text-item/text-select.directive";
+import {ResultsReviewComponent} from "./annotation/results-review/results-review.component";
+import {NerJobTextEditDialog} from "./management/ner-job-text-edit/ner-job-text-edit-dialog.component";
+import {NerJobEditComponent} from "./management/ner-jobs/ner-job-edit/ner-job-edit.component";
+import {NerJobsComponent} from "./management/ner-jobs/ner-jobs.component";
+import {NerJobTextAddComponent} from "./management/ner-job-text-edit/ner-job-text-add.component";
+import {NerJobTextsListComponent} from "./management/ner-job-texts-list/ner-job-texts-list.component";
+import {TermsAnnotationsService} from "./annotation/text-item/terms-annotations.service";
+import {NerJobsService} from "./management/ner-jobs/ner-jobs.service";
 
 @NgModule({
     declarations: [
