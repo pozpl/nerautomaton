f776bc893f0efd449653a74db0dd8e69d2a42596
Pavel Pozdnyak <pozdpl@gmail.com>
2020-05-24 19:31:41 +1200

    Controller to list annotation tasks to user.

---
+++ src/main/java/com/pozpl/nerannotator/ner/annotation/tasks/UserAnnotationTaskListController.java
@@ -0,0 +1,34 @@
+package com.pozpl.nerannotator.ner.annotation.tasks;
+
+import com.pozpl.nerannotator.auth.dao.model.User;
+import com.pozpl.nerannotator.ner.management.job.NerJobDto;
+import com.pozpl.nerannotator.shared.exceptions.NerServiceException;
+import com.pozpl.nerannotator.shared.exceptions.NerWebException;
+import com.pozpl.nerannotator.shared.pagination.PageDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/ner/user/tasks")
+public class UserAnnotationTaskListController {
+
+	private final IAnnotationTaskListService annotationTaskListService;
+
+	@Autowired
+	public UserAnnotationTaskListController(IAnnotationTaskListService annotationTaskListService) {
+		this.annotationTaskListService = annotationTaskListService;
+	}
+
+	@GetMapping("/list")
+	public PageDto<NerJobDto> list(@RequestParam(name = "page", required = false, defaultValue = "1") Integer page,
+							User user) {
+		try{
+			return annotationTaskListService.listAvailableJobs(user, page);
+		}   catch (NerServiceException e){
+			throw new NerWebException(e);
+		}
+	}
+}
