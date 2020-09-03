7b4d3c1dad1873e30c95c2178a56fbda99316fb0
Pavel Pozdnyak <pozdpl@gmail.com>
2020-09-03 22:39:59 +1200

    Deleted commented code from the security configuration

--- src/main/java/com/pozpl/nerannotator/config/SecurityConfiguration.java
+++ src/main/java/com/pozpl/nerannotator/config/SecurityConfiguration.java
@@ -14,7 +14,6 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
-//@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
@@ -33,54 +32,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.and().logout()
 				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));//explicit logout because I can
 
-		/*
-		http.
-                authorizeRequests()
-                .antMatchers("/").permitAll()
-                .antMatchers("/login").permitAll()
-                .antMatchers("/registration").permitAll()
-                .antMatchers("/admin/**").hasAuthority("ADMIN").anyRequest()
-                .authenticated().and().csrf().disable().formLogin()
-                .loginPage("/login").failureUrl("/login?error=true")
-                .defaultSuccessUrl("/admin/home")
-                .usernameParameter("email")
-                .passwordParameter("password")
-                .and().logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/").and().exceptionHandling()
-                .accessDeniedPage("/access-denied");
-
-		 */
 	}
 
-//	@Override
-//	public void configure(WebSecurity web) throws Exception {
-//		web.ignoring()
-//			.antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
-//	}
 
-//	@Bean
-//	public CorsFilter corsFilter() {
-//		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//		CorsConfiguration config = new CorsConfiguration();
-//		config.setAllowCredentials(true);
-//		config.addAllowedOrigin("*");
-//		config.addAllowedHeader("*");
-//		config.addAllowedMethod("OPTIONS");
-//		config.addAllowedMethod("GET");
-//		config.addAllowedMethod("POST");
-//		config.addAllowedMethod("PUT");
-//		config.addAllowedMethod("DELETE");
-//		source.registerCorsConfiguration("/**", config);
-//		return new CorsFilter(source);
-//	}
 
 	@Autowired
 	private MyUserDetailService userDetailsService;
 
 	@Override
-	protected void configure(AuthenticationManagerBuilder auth)
-			throws Exception {
+	protected void configure(AuthenticationManagerBuilder auth)  throws Exception {
 		auth.authenticationProvider(authenticationProvider());
 	}
 
