cef58ceec7099fc9701c1250e2e8195f9b8391df
Pavel Pozdnyak <pozdpl@gmail.com>
2020-09-09 21:27:06 +1200

    Fix for generation strategy to work properly with Postgres

--- src/main/java/com/pozpl/nerannotator/auth/dao/model/Role.java
+++ src/main/java/com/pozpl/nerannotator/auth/dao/model/Role.java
@@ -8,7 +8,7 @@ import java.util.Collection;
 public class Role {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@Enumerated(EnumType.STRING)
