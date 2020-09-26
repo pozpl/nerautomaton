734a5476f3a6b6c20cb108ce080c77a3d77c294b
Pavel Pozdnyak <pozdpl@gmail.com>
2020-09-27 12:13:19 +1300

    No longer using layers for application, trying to switch to verticals.

--- src/main/java/com/pozpl/nerannotator/config/AppConfig.java
+++ src/main/java/com/pozpl/nerannotator/config/AppConfig.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableTransactionManagement
-@EnableJpaRepositories( basePackages = "com.pozpl.nerannotator.persistence.dao")
+@EnableJpaRepositories( basePackages = "com.pozpl.nerannotator")
 public class AppConfig {
 }
