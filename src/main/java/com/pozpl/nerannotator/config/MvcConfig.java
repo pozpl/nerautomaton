694bea7f9c25c6ff699e1524ca4ab3aa1bc19936
Pavel Pozdnyak <pozdpl@gmail.com>
2020-09-29 22:56:00 +1300

    Build app into single uber jar

--- src/main/java/com/pozpl/nerannotator/config/MvcConfig.java
+++ src/main/java/com/pozpl/nerannotator/config/MvcConfig.java
@@ -4,15 +4,24 @@ package com.pozpl.nerannotator.config;
 import com.pozpl.nerannotator.shared.resolvers.LoggedInUserResolver;
 import com.pozpl.nerannotator.auth.dao.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ResourceProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
+import java.io.IOException;
+import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
 import java.util.List;
 
 @Configuration
 @EnableWebMvc
+@EnableConfigurationProperties({ResourceProperties.class})
 public class MvcConfig implements WebMvcConfigurer {
 
 	UserRepository userRepository;
@@ -26,5 +35,53 @@ public class MvcConfig implements WebMvcConfigurer {
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
 		argumentResolvers.add(new LoggedInUserResolver(userRepository));
 	}
+
+
+	static final String[] STATIC_RESOURCES = new String[]{
+			"/**/*.css",
+			"/**/*.html",
+			"/**/*.js",
+			"/**/*.json",
+			"/**/*.bmp",
+			"/**/*.jpeg",
+			"/**/*.jpg",
+			"/**/*.png",
+			"/**/*.ttf",
+			"/**/*.eot",
+			"/**/*.svg",
+			"/**/*.woff",
+			"/**/*.woff2"
+	};
+
+	@Autowired
+	private ResourceProperties resourceProperties = new ResourceProperties();
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		//Add all static files
+//		Integer cachePeriod = Long.valueOf(resourceProperties.getCache().getPeriod().get(ChronoUnit.SECONDS)).intValue();
+		registry.addResourceHandler(STATIC_RESOURCES)
+				.addResourceLocations(resourceProperties.getStaticLocations());
+				//.setCachePeriod(cachePeriod);
+
+		//Create mapping to index.html for Angular HTML5 mode.
+//		String[] indexLocations = getIndexLocations();
+//		registry.addResourceHandler("/**")
+//				.addResourceLocations(indexLocations)
+////				.setCachePeriod(cachePeriod)
+//				.resourceChain(true)
+//				.addResolver(new PathResourceResolver() {
+//					@Override
+//					protected Resource getResource(String resourcePath, Resource location) throws IOException {
+//						return location.exists() && location.isReadable() ? location : null;
+//					}
+//				});
+	}
+
+//	private String[] getIndexLocations() {
+//		return Arrays.stream(resourceProperties.getStaticLocations())
+//				.map((location) -> location + "index.html")
+//				.toArray(String[]::new);
+//	}
 
 }
--- src/main/java/com/pozpl/nerannotator/system/EntryController.java
+++ src/main/java/com/pozpl/nerannotator/system/EntryController.java
@@ -2,13 +2,21 @@ package com.pozpl.nerannotator.system;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class EntryController {
 
 
 	@RequestMapping("/")
-	public String getRoot(){
-		return "forward:index.html";
+	public RedirectView getRoot(){
+		return new RedirectView("index.html");
 	}
+
+	@RequestMapping("/#/*")
+	public RedirectView handleReload(){
+		return new RedirectView("/index.html");
+	}
+
+
 }
