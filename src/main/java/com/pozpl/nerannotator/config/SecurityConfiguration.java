c0022f759ab1ec063354b7b4902a5660fce182b3
Pavel Pozdnyak <pozdpl@gmail.com>
2021-01-21 23:29:19 +1300

    Role admin

--- src/main/java/com/pozpl/nerannotator/config/SecurityConfiguration.java
+++ src/main/java/com/pozpl/nerannotator/config/SecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -19,6 +20,7 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Autowired
@@ -37,7 +39,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.authorizeRequests()
 				.antMatchers("/index.html", "/", "/home", "/login", "/api/auth/**")
 				.permitAll()
-				.antMatchers("/admin/**").hasAuthority("ADMIN")
+				.antMatchers("/admin/**").hasAuthority("ROLE_ADMIN")
 				.antMatchers("/*.js", "/*.js.map" ,"/*.css", "/*.css.map", "/*.ico").permitAll()
 				.anyRequest().authenticated()
 				.and().csrf()
